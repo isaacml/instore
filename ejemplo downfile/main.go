4af95be6a9f22c1ab0300de03bcb0122eefc731c
Isaac ML <isaac@todostreaming.es>
2017-02-22 17:12:42 +0100

    Quitando ejemplos que ya no hacen falta

--- ejemplo downfile/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/isaacml/instore/libs"
-)
-
-func main() {
-	libs.DownloadFile("http://www.dreamscene.org/wallpapers/Aquadream.jpg", "/home/isaac/nuevas pruebas/new.jpg", 2, 100)
-}
