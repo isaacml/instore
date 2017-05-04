2c98eb4efdd635f455e8f2d9f4ada5b68a61c43f
Isaac ML <isaac@todostreaming.es>
2017-05-04 19:17:20 +0200

    Sirviendo mp3 desde server externo

--- server_externo/fileserver.go
+++ server_externo/fileserver.go
@@ -18,14 +18,9 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("... Buscando fichero: ", namefile)
 	fileinfo, err := os.Stat(namefile)
 	if err != nil {
-		// fichero no existe
-		//fmt.Println("404 - Fichero no encontrado: ",namefile)
 		http.NotFound(w, r)
 		return
 	} else if fileinfo.IsDir() {
-		// es un directorio, luego le añadimos index.html
-		//namefile = namefile + "/" + first_page + ".html"
-		//fmt.Println("/ - Entramos en el Directorio Buscando el fichero: ",namefile)
 		_, err2 := os.Stat(namefile)
 		if err2 != nil {
 			//fmt.Println("404 - Fichero no encontrado: ",namefile)
@@ -33,4 +28,11 @@ func root(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	fr, errn := os.Open(namefile)
+	if errn != nil {
+		//fmt.Printf("[root(4)] - Error de acceso al fichero: %s\n",namefile)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	http.ServeFile(w, r, namefile)
 }
