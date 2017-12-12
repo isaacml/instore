dd18150d57f473fdb85107f6ebc7ad88713a344c
Vidanio <info@vidanio.com>
2017-12-12 11:06:59 +0100

    cambios en los configs

--- server_externo/config.go
+++ server_externo/config.go
@@ -1,8 +1,9 @@
 package main
 
 const (
-	http_port            = "9999"                                               // puerto del server HTTP
-	logFile              = "/home/isaac/INSTORE_SQL/pruebas/server_externo.log" //ruta del archivo de errores
-	publi_files_location = "/home/isaac/INSTORE_SQL/pruebas/Publi/"             //ruta donde se van a alojar la publicidad subida por parte del admin externo
-	msg_files_location   = "/home/isaac/INSTORE_SQL/pruebas/Messages/"          //ruta donde se van a alojar los mensajes subidos por parte del admin externo
+	http_port            = "9999"                                  // puerto del server HTTP
+	logFile              = "/usr/local/bin/log/server_externo.log" //ruta del archivo de errores
+	publi_files_location = "/usr/local/bin/Publi/"                 //ruta donde se van a alojar la publicidad subida por parte del admin externo
+	msg_files_location   = "/usr/local/bin/Messages/"              //ruta donde se van a alojar los mensajes subidos por parte del admin externo
+	sql_file             = "/usr/local/bin/servext.db"             // fichero SQL del servidor interno
 )
--- server_interno/config.go
+++ server_interno/config.go
@@ -1,10 +1,10 @@
 package main
 
 const (
-	http_port            = "9898"                                               // puerto del server HTTP
-	logFile              = "/home/isaac/INSTORE_SQL/pruebas/server_interno.log" // ruta del archivo de errores
-	serverRoot           = "/home/isaac/INSTORE_SQL/pruebas/serverint.reg"      // fichero que contiene la ruta del servidor externo
-	publi_files_location = "/home/isaac/INSTORE_SQL/pruebas/PubliInt/"          // ruta donde se van a alojar la publicidad del server interno
-	msg_files_location   = "/home/isaac/INSTORE_SQL/pruebas/MessagesInt/"       // ruta donde se van a alojar los mensajes del server interno
-	sql_file			 = "/home/isaac/INSTORE_SQL/pruebas/servint.db"			// fichero SQL del servidor interno
+	http_port            = "9898"                                   // puerto del server HTTP
+	logFile              = "/usr/local/bin/logs/server_interno.log" // ruta del archivo de errores
+	serverRoot           = "/usr/local/bin/serverint.reg"           // fichero que contiene la ruta del servidor externo
+	publi_files_location = "/usr/local/bin/PubliInt/"               // ruta donde se van a alojar la publicidad del server interno
+	msg_files_location   = "/usr/local/bin/MessagesInt/"            // ruta donde se van a alojar los mensajes del server interno
+	sql_file             = "/usr/local/bin/servint.db"              // fichero SQL del servidor interno
 )
