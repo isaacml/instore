b0b7ec89feacfda068a733731880c13843936f98
Isaac ML <isaac@todostreaming.es>
2017-12-12 13:22:53 +0100

    Cambio del fichero log

--- server_externo/config.go
+++ server_externo/config.go
@@ -1,9 +1,9 @@
 package main
 
 const (
-	http_port            = "9999"                                  // puerto del server HTTP
-	logFile              = "/usr/local/bin/log/server_externo.log" //ruta del archivo de errores
-	publi_files_location = "/usr/local/bin/Publi/"                 //ruta donde se van a alojar la publicidad subida por parte del admin externo
-	msg_files_location   = "/usr/local/bin/Messages/"              //ruta donde se van a alojar los mensajes subidos por parte del admin externo
-	sql_file             = "/usr/local/bin/servext.db"             // fichero SQL del servidor interno
+	http_port            = "9999"                                   // puerto del server HTTP
+	logFile              = "/usr/local/bin/logs/server_externo.log" //ruta del archivo de errores
+	publi_files_location = "/usr/local/bin/Publi/"                  //ruta donde se van a alojar la publicidad subida por parte del admin externo
+	msg_files_location   = "/usr/local/bin/Messages/"               //ruta donde se van a alojar los mensajes subidos por parte del admin externo
+	sql_file             = "/usr/local/bin/servext.db"              // fichero SQL del servidor interno
 )
